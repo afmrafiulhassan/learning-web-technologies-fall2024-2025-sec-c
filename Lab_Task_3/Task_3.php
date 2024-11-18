48ebfcedc53ed85f7e0e753c6edb9217ebadf5c6
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-18 22:00:37 +0600

    Add files via upload

---
+++ Lab_Task_3/Task_3.php
@@ -0,0 +1,14 @@
+<?php
+
+    $number = 10;
+
+    echo "The entered number is: " . $number . "<br>";
+
+    if ($number % 2 == 0) {
+        echo "The number $number is even.";
+    } else {
+        echo "The number $number is odd.";
+    }
+
+
+?>
