7d5ccd93108dd861d3438bc50979b03fcabbd5bd
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-18 21:59:32 +0600

    Add files via upload

---
+++ Lab_Task_3/Task_1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $length=15;
+    $width=20;
+
+    $area = $length * $width;
+    $perimeter = 2 * ($length + $width);
+
+    echo("The area of a Rectangle: ". $area . "<br>");
+    echo("The area of the Perimeter: ". $perimeter . "<br>")
+
+?>
