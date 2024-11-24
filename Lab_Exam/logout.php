cc1860856bbbc7073e4a85d5d9b374969c9d54b9
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-24 16:52:31 +0600

    Update logout.php

--- Lab_Exam/logout.php
+++ Lab_Exam/logout.php
@@ -1,8 +1,8 @@
 <?php
 
     session_start();
-    //unset($_SESSION['status']);
-    session_destroy();
+    unset($_SESSION['status']);
+    //session_destroy();
     header('location: login.html');
 
 ?>
