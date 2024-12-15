25c8ac09564dafe0fd8973916e2813f13d23ca33
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-12-15 16:44:35 +0600

    Add files via upload

---
+++ Lab_Exam_2/view_users.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Users</title>
+    </head>
+    <body>
+        <table align="center" border="1" cellspacing="0">
+            <tr>
+                <td colspan="3">
+                    Users
+                </td>
+            </tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>USER TYPE</td>
+            
+        </table>
+    </body>
+</html>
