22ce635528d430a02ff395fcfd726fd53d419237
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2025-01-12 16:43:20 +0600

    Add files via upload

---
+++ Final_Lab_Exam/view/delete_user.php
@@ -0,0 +1,14 @@
+<?php
+require_once("../model/user_model.php");
+$name = $_REQUEST['uname'];
+
+$result = delete_user($uname);
+
+if($result){
+    echo "Delete successfull.";
+}
+else{
+    echo "Error!";
+}
+
+?>
